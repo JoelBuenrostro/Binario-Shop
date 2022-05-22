a7c4aea699d0915d31ff21b5cd6b1114caad928a
Joel Buenrostro <joel.buenrostro@intel.com>
2022-05-22 09:43:19 -0500

    Updates

---
+++ precache-manifest.fc3f323cf534b439d8ce2382de991e0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3bdeb3702fb82f1af07bca4bc73cbf",
+    "url": "/JoelBuenrostro/Binario-Shop/index.html"
+  },
+  {
+    "revision": "629fae0c4178088c2e0d",
+    "url": "/JoelBuenrostro/Binario-Shop/static/css/main.0838601a.chunk.css"
+  },
+  {
+    "revision": "f5040220f9e4a6df2449",
+    "url": "/JoelBuenrostro/Binario-Shop/static/js/2.7d3498ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/JoelBuenrostro/Binario-Shop/static/js/2.7d3498ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629fae0c4178088c2e0d",
+    "url": "/JoelBuenrostro/Binario-Shop/static/js/main.a4a2aa22.chunk.js"
+  },
+  {
+    "revision": "6cb5b6a2d8c7b2da3eda",
+    "url": "/JoelBuenrostro/Binario-Shop/static/js/runtime-main.1193ddac.js"
+  }
+]);
