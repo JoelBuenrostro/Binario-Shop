c67af6216c28bdfe5f1d9e921177281e42af07b4
Joel Buenrostro <joel.buenrostro@intel.com>
2022-05-22 09:00:22 -0500

    Configuracion de React
    Primer vista de pagina de inicio  configuracion inicial del ambiente para React

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Audifonos Airpods inalambricos Bluetooth',
+    image: '/images/airpods.jpg',
+    description:
+      'La tecnología Bluetooth le permite conectarlo con dispositivos compatibles de forma inalámbrica El audio AAC de alta calidad ofrece una experiencia auditiva inmersiva El micrófono incorporado le permite recibir llamadas mientras trabaja',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 1792.42,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'iPhone 11 Pro Memoria 256GB',
+    image: '/images/phone.jpg',
+    description:
+      'Presentamos el iPhone 11 Pro. Un sistema transformador de triple cámara que agrega toneladas de capacidad sin complejidad. Un salto sin precedentes en la duración de la batería',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 11950.60,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    _id: '3',
+    name: 'Camara Cannon EOS 80D DSLR',
+    image: '/images/camera.jpg',
+    description:
+      'Caracterizada por especificaciones de imagen versátiles, la Canon EOS 80D se aclara aún más utilizando un par de sistemas de enfoque robustos y un diseño intuitivo.',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 918523.54,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: '4',
+    name: 'Sony Playstation 4 Pro Version Blanca',
+    image: '/images/playstation.jpg',
+    description:
+      'El mejor centro de entretenimiento en el hogar comienza con PlayStation. Ya sea que te gusten los juegos, las películas HD, la televisión, la música',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 7967.00,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    _id: '5',
+    name: 'Mouse Gaming Logitech G-Series',
+    image: '/images/mouse.jpg',
+    description:
+      'Controle mejor sus juegos con este mouse para juegos Logitech LIGHTSYNC. Los seis botones programables permiten la personalización para una experiencia de juego fluida',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 995.70,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    _id: '6',
+    name: 'Amazon Echo Dot 3rd Generacion',
+    image: '/images/alexa.jpg',
+    description:
+      'Conoce a Echo Dot: nuestro altavoz inteligente más popular con un diseño de tela. Es nuestro altavoz inteligente más compacto que encaja perfectamente en un espacio reducido',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 597.34,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4,
+  },
+]
+
+export default products
